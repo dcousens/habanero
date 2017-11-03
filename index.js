632753efd55f8ed8b1bb03b86531ac52318a4169
Daniel Cousens <github@dcousens.com>
2017-11-03 21:43:20 +1100

    expose zero work factor kdf

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function prepare (seed, passcode) {
   return crypto.hmac256(seed, passcode)
 }
 
-module.exports = { prepare }
+// XXX: near zero work-factor, suitable for low power devices, assumes cryptographically random seed/pepper
+function kdf (seed, pepper, passcode) {
+  if (!crypto.isBufferN(seed, 32)) throw new TypeError('Bad seed')
+  if (!crypto.isBufferN(pepper, 32)) throw new TypeError('Bad pepper')
+  if (!isPasscode(passcode)) throw new TypeError('Bad passcode')
+  let key = Buffer.concat([seed, pepper], 64)
+  return crypto.hmac256(key, passcode)
+}
+
+module.exports = { prepare, kdf }
