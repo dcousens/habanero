a344c2ba96597089840549429333fa7d33bca23e
Daniel Cousens <github@dcousens.com>
2017-11-13 15:10:02 +1100

    seed is salt, not seed

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ function isPasscode (x) {
   return typeof x === 'string' && x.length >= 4
 }
 
-function prepare (seed, passcode) {
-  if (!crypto.isBufferN(seed, 32)) throw new TypeError('Bad seed')
+function prepare (salt, passcode) {
+  if (!crypto.isBufferN(salt, 32)) throw new TypeError('Bad salt')
   if (!isPasscode(passcode)) throw new TypeError('Bad passcode')
-  return crypto.hmac256(seed, passcode)
+  return crypto.hmac256(salt, passcode)
 }
 
-// XXX: near zero work-factor, assumes cryptographically random seed and pepper
-function kdf (seed, pepper, passcode) {
-  if (!crypto.isBufferN(seed, 32)) throw new TypeError('Bad seed')
+// XXX: near zero work-factor, assumes cryptographically random salt and pepper
+function kdf (salt, pepper, passcode) {
+  if (!crypto.isBufferN(salt, 32)) throw new TypeError('Bad salt')
   if (!crypto.isBufferN(pepper, 32)) throw new TypeError('Bad pepper')
   if (!isPasscode(passcode)) throw new TypeError('Bad passcode')
-  let key = Buffer.concat([seed, pepper], 64)
+  let key = Buffer.concat([salt, pepper], 64)
   return crypto.hmac256(key, passcode)
 }
 
